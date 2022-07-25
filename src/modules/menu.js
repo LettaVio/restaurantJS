0590ecc9ff7eb8d1245d20645d9ac689e38bffb3
LettaVio <viollogika@gmail.com>
2022-07-25 23:13:11 +0300

    dinamic content from json

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,13 +1,62 @@
 export const menu = () => {
-    const restaurant = 'tanuki';
+    const cardMenu = document.querySelector('.cards-menu');
+
+    const changeInfo = (restaurant) => {
+        const restaurantTitle = document.querySelector('.restaurant-title');
+        restaurantTitle.textContent = restaurant.name;
+
+        const rating = document.querySelector('.rating');
+        rating.textContent = restaurant.stars;
+
+        const price = document.querySelector('.price');
+        price.textContent = `От ${restaurant.price} ₽`;
+
+        const category = document.querySelector('.category');
+        category.textContent = restaurant.kitchen;
+
+    }
 
     const renderItems = (data) => {
-        data.forEach(element => {
-            console.log(element);
+        data.forEach(({ description, id, image, name, price }) => {
+
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+                    <img src="${image}" alt="image" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">${name}</h3>
+							</div>
+							<!-- /.card-heading -->
+							<div class="card-info">
+								<div class="ingredients">${description}
+								</div>
+							</div>
+							
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">${price} ₽</strong>
+							</div>
+						</div>
+                `;
+
+            cardMenu.append(card);
         });
     }
-    fetch(`./db/${restaurant}.json`)
-        .then(response => response.json())
-        .then(data => renderItems(data))
-        .catch(error => console.log(error))
+
+    if (localStorage.getItem('restaurantJSON')) {
+        const restaurant = JSON.parse(localStorage.getItem('restaurantJSON'));
+        changeInfo(restaurant);
+        fetch(`./db/${restaurant.products}`)
+            .then(response => response.json())
+            .then(data => renderItems(data))
+            .catch(error => console.log(error))
+    }
+    else {
+        window.location.href = '/';
+    }
+
 }
--- src/modules/partners.js
+++ src/modules/partners.js
@@ -1,7 +1,45 @@
 export const partners = () => {
+
+    const cardRestaurants = document.querySelector('.cards-restaurants');
+
+
     const renderItems = (data) => {
-        data.forEach(item => {
-            console.log(item);
+        data.forEach((item) => {
+            const { image, kitchen, name, price, products, stars, time_of_delivery } = item;
+            const a = document.createElement('a');
+            a.setAttribute('href', '/restaurant.html');
+            a.classList.add('card');
+            a.classList.add('card-restaurant');
+            a.dataset.products = products;
+            a.innerHTML = `
+            <img src="${image}" alt="image" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title">${name}</h3>
+								<span class="card-tag tag">${time_of_delivery} мин</span>
+							</div>
+							<div class="card-info">
+								<div class="rating">
+									${stars}
+								</div>
+								<div class="price"> ${price} ₽ </div>
+								<div class="category">${kitchen}</div>
+							</div>							
+						</div>
+						
+            `
+            a.addEventListener('click', (e) => {
+                e.preventDefault();
+                if (localStorage.getItem('user')) {
+                    localStorage.setItem('restaurantJSON', JSON.stringify(item));
+                    window.location.href = '/restaurant.html';
+                }
+                else {
+                    document.querySelector(".modal-auth").style.display = 'flex';
+                }
+            })
+            cardRestaurants.append(a);
+
         });
 
     }

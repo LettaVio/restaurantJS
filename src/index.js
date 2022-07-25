0985a80257d5e74d7a9048d08ca003f66f6a0005
LettaVio <viollogika@gmail.com>
2022-07-25 20:04:07 +0300

    firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { menu } from "./modules/menu";
 import { auth } from "./modules/auth";
-import { partners } from "./modules/partners";
+
 
 auth()
 menu()
-partners()
--- src/modules/partners.js
+++ src/modules/partners.js
@@ -1,11 +1,18 @@
 export const partners = () => {
     const renderItems = (data) => {
-        data.forEach(element => {
-            console.log(element);
+        data.forEach(item => {
+            console.log(item);
         });
+
     }
-    fetch(`./db/${restaurant}.json`)
-        .then(response => response.json())
-        .then(data => renderItems(data))
-        .catch(error => console.log(error))
+
+    //fetch('./db/partners.json')
+    fetch('https://restaurant-d24f2-default-rtdb.firebaseio.com/db/partners.json')
+        .then((response) => response.json())
+        .then((data) => {
+            renderItems(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
